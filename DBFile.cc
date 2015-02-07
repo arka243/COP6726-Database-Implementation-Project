c37f27ac4608b7299a20afa7289cf9fcd92fcfdb
arka243 <msarka.praharaj@gmail.com>
2015-02-06 22:46:33 -0500

    Modified header and class
    - Added GetNext functions
    - Added OPEN_STATUS boundary check to all functions
    - Modified Constructor

--- DBFile.cc
+++ DBFile.cc
@@ -15,8 +15,6 @@
 DBFile::DBFile()
 {
 	OPEN_STATUS = 0;
-	pageindex = (off_t)1;
-	recptr = 1;
 }
 
 
@@ -38,6 +36,8 @@ int DBFile::Open(char *filepath)
 	else
 	{
 		dbfile.Open(1, filepath);
+		pageindex = (off_t)1;
+		recptr = 1;
 		OPEN_STATUS = 1;
 	}
 	return(OPEN_STATUS);
@@ -58,65 +58,191 @@ int DBFile::Close()
 
 void DBFile::MoveFirst()
 {
-	pageindex = (off_t)(-1);
-	recptr = 1;
+	if(OPEN_STATUS == 1)
+	{
+		pageindex = (off_t)1;
+		recptr = 1;
+	}
+	else
+		cout << "\nFile not Open!";
 }
 
 
 void DBFile::Add(Record &addRec)
 {
-	Page pg;
-	pageindex = dbfile.GetLength();
-	dbfile.GetPage(&pg, pageindex);
-	pageindex--;
-	if(pg.Append(&addRec)==0)
+	if(OPEN_STATUS == 1)
 	{
+		Page pg;
+		pageindex = dbfile.GetLength();
+		dbfile.GetPage(&pg, pageindex);
+		pageindex--;
+		if(pg.Append(&addRec)==0)
+		{
+			pageindex++;
+			dbfile.AddPage(&pg, pageindex);
+			pg.EmptyItOut();
+			pg.Append(&addRec);
+		}
 		pageindex++;
-		dbfile.AddPage(&pg, pageindex);
-		pg.EmptyItOut();
-		pg.Append(&addRec);
+		dbfile.AddPage(&pg, pageindex);		
 	}
-	pageindex++;
-	dbfile.AddPage(&pg, pageindex);		
+	else
+		cout << "\nFile Not Open!";
 }
 
 
 int DBFile::GetNext(Record &fetchme)
 {
-
+	if(OPEN_STATUS == 1)
+	{
+		Record temprec;
+		Page pg, pg_store;
+		dbfile.GetPage(&pg, pageindex);
+		pageindex--;
+		int count = 0;
+		while(count < recptr)
+		{
+			if(pg.GetFirst(&temprec) == 1)
+			{
+				fetchme = temprec;
+				count++;
+				pg_store.Append(&temprec);
+			}
+			else 
+			{
+				cout << "You have reached the end of the page! Proceeding to the next page";
+				dbfile.AddPage(&pg_store, pageindex);
+				pg_store.EmptyItOut();
+				pageindex++;
+				if(pageindex > dbfile.GetLength())
+				{
+					cout << "You have reached the end of the file!";
+					pageindex--;
+					return 0;
+				}
+				else 
+				{
+					count = 0;
+					recptr = 1;
+				}	
+			}
+		}
+		recptr++;
+		while(pg.GetFirst(&temprec) != 0)
+		{
+			pg.GetFirst(&temprec);
+			pg_store.Append(&temprec);
+		}
+		pageindex++;
+		dbfile.AddPage(&pg_store, pageindex);
+		return 1;
+	}
+	else
+	{
+		cout << "\nFile not Open!";
+		return 0;
+	}
 }
 
 
 int DBFile::GetNext(Record &fetchRec, CNF &myCNF, Record &literal)
 {
-
+	if(OPEN_STATUS == 1)
+	{
+		int success = 0,count = 0;
+		Record temprec;
+		Page pg, pg_store;
+		ComparisonEngine myCompEngine;
+		while(success != 1)
+		{
+			dbfile.GetPage(&pg, pageindex);
+			pageindex--;
+			while(count < recptr)
+			{
+				if(pg.GetFirst(&temprec) == 1)
+				{
+					count++;
+					pg_store.Append(&temprec);
+				}
+				else 
+				{
+					cout << "You have reached the end of the page! Proceeding to the next page";
+					dbfile.AddPage(&pg_store, pageindex);
+					pg_store.EmptyItOut();
+					pageindex++;
+					if(pageindex > dbfile.GetLength())
+					{
+						cout << "You have reached the end of the file!";
+						pageindex--;
+						return 0;
+					}
+					else
+					{
+						count = 0;
+						recptr = 1;
+					}
+				}
+			}
+			while(!pg.GetFirst(&temprec) != 0)
+			{
+				pg_store.Append(&temprec);
+				recptr++;
+				if(myCompEngine.Compare(&temprec, &literal, &myCNF) == 1)
+				{
+					success = 1;
+					fetchRec = temprec;
+					recptr++;
+				}	
+			}
+			dbfile.AddPage(&pg_store, pageindex);
+			pg_store.EmptyItOut();
+			pageindex++;
+			if(pageindex > dbfile.GetLength())
+			{
+				cout << "You have reached the end of the file!";
+				pageindex--;
+				return 0;
+			}
+		}
+		return(success);
+	}
+	else
+	{
+		cout << "\nFile not Open";
+		return 0;
+	}
 }
 
 void DBFile::Load(Schema &mySchema, char *filepath)
 {
-	Record temp;
-	Page pg;
-	pageindex = dbfile.GetLength();
-	FILE *loadfile = fopen(filepath, "r");
-	if(loadfile!=NULL)
+	if(OPEN_STATUS == 1)
 	{
-		while(temp.SuckNextRecord(&mySchema, loadfile)==1)
+		Record temp;
+		Page pg;
+		pageindex = dbfile.GetLength();
+		FILE *loadfile = fopen(filepath, "r");
+		if(loadfile!=NULL)
 		{
-			int append_status = pg.Append(&temp);
-			if(append_status == 0)
+			while(temp.SuckNextRecord(&mySchema, loadfile)==1)
 			{
-				pageindex++;
-				dbfile.AddPage(&pg, pageindex);
-				pg.EmptyItOut();
-				pg.Append(&temp);
+				int append_status = pg.Append(&temp);
+				if(append_status == 0)
+				{
+					pageindex++;
+					dbfile.AddPage(&pg, pageindex);
+					pg.EmptyItOut();
+					pg.Append(&temp);
+				}
 			}
+			pageindex++;
+			dbfile.AddPage(&pg, pageindex);
 		}
-		pageindex++;
-		dbfile.AddPage(&pg, pageindex);
+		else
+		{
+			cout << "\nError: No file found at path:" << filepath;
+		}
+		fclose(loadfile);
 	}
 	else
-	{
-		cout << "\nError: No file found at path:" << filepath;
-	}
-	fclose(loadfile);
+		cout << "\nFile not Open";
 }
