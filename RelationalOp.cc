69450c0696679572133eebd2fd0e38ef04060ae3
arka243 <msarka.praharaj@gmail.com>
2015-04-30 07:10:37 -0400

    Completed implementation of RelationalOp.cc

--- RelationalOp.cc
+++ RelationalOp.cc
@@ -116,6 +116,250 @@ void Project :: Use_n_Pages(int n)
 	this->runlen = n;
 }
 
+
+//////////////////////   WRITE OUT OPERATION   //////////////////////
+
+void *SetupThreadWriteOut(void *arg)
+{
+	((WriteOut *)arg)->StartOpWriteOut();
+}
+
+void WriteOut :: StartOpWriteOut()
+{
+	Record temp;
+	while(in->Remove(&temp) == 1)
+		temp.WriteRecordsToFile(schema, file);
+	fclose(file);
+	in->ShutDown();
+}
+
+void WriteOut :: Run(Pipe &inPipe, FILE *outFile, Schema &mySchema)
+{
+	this->in = &inPipe;
+	this->file = outFile;
+	this->schema = &mySchema;
+	pthread_create(&myThread, NULL, SetupThreadWriteOut, (void *)this);
+}
+
+void WriteOut :: WaitUntilDone()
+{
+	pthread_join(myThread, NULL);
+}
+
+void WriteOut :: Use_n_Pages(int n)
+{
+	this->runlen = n;
+}
+
+
+//////////////////////   DUPLICATE REMOVAL OPERATION   //////////////////////
+
+void *SetupThreadDuplicateRemoval(void *arg)
+{
+	((DuplicateRemoval *)arg)->StartOpDuplicateRemoval();
+}
+
+void DuplicateRemoval :: StartOpDuplicateRemoval()
+{
+	Record temp, next;
+	OrderMaker *sortorder = new OrderMaker(schema);
+	Pipe *pipe = new Pipe(100);
+	BigQ bigq(*in, *pipe, *sortorder, 10);
+	ComparisonEngine comp;
+	pipe->Remove(&next);
+	temp.Copy(&next);
+	out->Insert(&next);
+	while(pipe->Remove(&next) == 1)
+	{
+		if(comp.Compare(&temp, &next, sortorder) != 0)
+		{
+			temp.Copy(&next);
+			out->Insert(&next);
+		}
+	}
+	in->ShutDown();
+	out->ShutDown();
+	delete pipe;	
+}
+
+void DuplicateRemoval :: Run(Pipe &inPipe, Pipe &outPipe, Schema &mySchema)
+{
+	this->in = &inPipe;
+	this->out = &outPipe;
+	this->schema = &mySchema;
+	pthread_create(&myThread, NULL, SetupThreadDuplicateRemoval, (void *)this);
+}
+
+void DuplicateRemoval :: WaitUntilDone()
+{
+	pthread_join(myThread, NULL);
+}
+
+void DuplicateRemoval :: Use_n_Pages(int n)
+{
+	this->runlen = n;
+}
+
+
+//////////////////////   SUM OPERATION   //////////////////////
+
+void *SetupThreadSum(void *arg)
+{
+	((Sum *)arg)->StartOpSum();
+}
+
+void Sum :: StartOpSum()
+{
+	Record temp, groupSum;
+	Type type;
+	int sumIntRec, sumIntTotal = 0;
+	double sumDblRec, sumDblTotal = 0;
+	while(in->Remove(&groupSum) == 1)
+	{
+		compute->Apply(groupSum, sumIntRec, sumDblRec);
+		if(compute->returnsInt == 1)
+		{
+			type = Int;
+			sumIntTotal = sumIntTotal + sumIntRec;
+		}
+		else
+		{
+			type = Double;
+			sumDblTotal = sumDblTotal + sumDblRec;
+		}
+	}
+	temp.CreateNewRecord(type, sumIntTotal, sumDblTotal);
+	out->Insert(&temp);
+	out->ShutDown();
+}
+
+void Sum :: Run(Pipe &inPipe, Pipe &outPipe, Function &computeMe)
+{
+	this->in = &inPipe;
+	this->out = &outPipe;
+	this->compute = &computeMe;
+	pthread_create(&myThread, NULL, SetupThreadSum, (void *)this);
+}
+
+void Sum :: WaitUntilDone()
+{
+	pthread_join(myThread, NULL);
+}
+
+void Sum :: Use_n_Pages(int n)
+{
+	this->runlen = n;
+}
+
+
+//////////////////////   GROUPBY OPERATION   //////////////////////
+
+void *SetupThreadGroupBy(void *arg)
+{
+	((GroupBy *)arg)->StartOpGroupBy();
+}
+
+void GroupBy :: StartOpGroupBy()
+{
+	Record recArr[2];
+	Record *start = NULL, *end = NULL;
+	Record *temp = new Record;
+	Record *dump = new Record;
+	Type type;
+	Pipe *pipe = new Pipe(100);
+	BigQ bigq(*in, *pipe, *sortorder, 10);
+	ComparisonEngine comp;
+	int arrbound = (sortorder->numAtts)+1;
+	int groupId = 0, sumIntTotal = 0, sumIntRec, attsArr[arrbound]; 
+	double sumDblTotal=0, sumDblRec;
+	
+	attsArr[0] = 0;
+	for(int i = 1; i < arrbound; i++)
+		attsArr[i] = sortorder->whichAtts[i-1];
+	
+	while(pipe->Remove(&recArr[groupId%2]) == 1)
+	{
+		start = end;
+		end = &recArr[groupId%2];
+		if(start != NULL && end != NULL)
+		{
+			if(comp.Compare(start, end, sortorder) != 0)
+			{
+				compute->Apply(*start, sumIntRec, sumDblRec);
+				if(compute->returnsInt == 1)
+				{
+					type = Int;
+					sumIntTotal = sumIntTotal + sumIntRec;
+				}
+				else
+				{
+					type = Double;
+					sumDblTotal = sumDblTotal + sumDblRec;
+				}
+				int startint = ((int *)start->bits)[1]/sizeof(int) - 1;
+				dump->CreateNewRecord(type, sumIntTotal, sumDblTotal);
+				temp->MergeRecords(dump, start, 1, startint, attsArr, arrbound, 1);
+				out->Insert(temp);
+				sumIntTotal = 0;
+				sumDblTotal = 0;
+			}
+			else
+			{
+				compute->Apply(*start, sumIntRec, sumDblRec);
+				if(compute->returnsInt == 1)
+				{
+					type = Int;
+					sumIntTotal = sumIntTotal + sumIntRec;
+				}
+				else
+				{
+					type = Double;
+					sumDblTotal = sumDblTotal + sumDblRec;
+				}
+			}
+		}
+		groupId++;
+	}
+	
+	compute->Apply(*end, sumIntRec, sumDblRec);
+	if(compute->returnsInt == 1)
+	{
+		type = Int;
+		sumIntTotal = sumIntTotal + sumIntRec;
+	}
+	else
+	{
+		type = Double;
+		sumDblTotal = sumDblTotal + sumDblRec;
+	}
+	int startint = ((int *)start->bits)[1]/sizeof(int) - 1;
+	dump->CreateNewRecord(type, sumIntTotal, sumDblTotal);
+	temp->MergeRecords(dump, end, 1, startint, attsArr, arrbound, 1);
+	out->Insert(temp);
+	
+	out->ShutDown();
+}
+
+void GroupBy :: Run(Pipe &inPipe, Pipe &outPipe, OrderMaker &groupAtts, Function &computeMe)
+{
+	this->in = &inPipe;
+	this->out = &outPipe;
+	this->sortorder = &groupAtts;
+	this->compute = &computeMe;
+	pthread_create(&myThread, NULL, SetupThreadGroupBy, (void *)this);
+}
+
+void GroupBy :: WaitUntilDone()
+{
+	pthread_join(myThread, NULL);
+}
+
+void GroupBy :: Use_n_Pages(int n)
+{
+	this->runlen = n;
+}
+
+
 //////////////////////   JOIN OPERATION   //////////////////////
 
 void *SetupThreadJoin(void *arg)
